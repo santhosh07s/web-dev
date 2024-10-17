90d3ae74f47ee90b31571f4a0b045cf998c59527
santhosh07s <santhoshperumal74r@gmail.com>
2024-10-17 19:27:01 +0530

    empty routes in different pages

---
+++ routes/course.js
@@ -0,0 +1,16 @@
+function courseroutes(app){
+        app.get("/preview-course", (req,res) =>{
+
+        })
+        
+        
+        app.post("/my-course", (req,res) =>{
+        
+        })
+
+}
+
+
+module.exports = {
+    courseroutes : courseroutes
+}
---
+++ routes/users.js
@@ -0,0 +1,21 @@
+function userroutes (app){
+        app.post("/signup", (req,res) =>{
+            
+
+        })
+        
+        app.post("/signin", (req,res) =>{
+
+        })
+        
+        app.post("/profile", (req,res) =>{
+
+        })
+    
+}
+
+
+
+module.exports = {
+    userroutes: userroutes
+}
